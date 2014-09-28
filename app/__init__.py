4a12f833c9ce0ca544b5fefc0ab5549a826eac44
SebastianMerz <Sebastian@sebastianmerz.com>
2014-09-27 21:37:18 -0700

    fixed?{

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from pymarkovchain import MarkovChain
 
 app = Flask(__name__)
-mc1 = MarkovChain('./posts')
-mc2 = MarkovChain('./comments')
+mc1 = MarkovChain('/var/www/personal/markov/posts')
+mc2 = MarkovChain('/var/www/personal/markov/comments')
 
 from app import views
